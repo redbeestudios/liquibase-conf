34396ddd3aa1e5306eb93a6a3b9a9a4c59c9a994
martin.britez <martin.britez@gmail.com>
2022-11-18 10:58:28 -0300

    seed

---
+++ src/main/java/com/redbeeconf/mongoliquibase/MongoliquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.redbeeconf.mongoliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoliquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoliquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redbeeconf/mongoliquibase/MongoliquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redbeeconf.mongoliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoliquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
